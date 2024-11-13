1fedfa002a8673af1c02048cc0929f0bbd1ee059
Daniil Vasylenko <daniel.vsln@gmail.com>
2024-11-13 17:40:28 +0200

    feat: add github workspace

--- locals.tf
+++ locals.tf
@@ -10,7 +10,13 @@ locals {
       description         = "Workspace description"
       execution_mode      = "remote"
       project_id          = module.project["devops-course-project"].id
-      vcs_repo_identifier = "daniel-vsln/devops-course-terraform-tfe"
+      vcs_repo_identifier = "${var.github_organization_name}/devops-course-terraform-tfe"
+    }
+    "devops-course-github" = {
+      description         = "Workspace description"
+      execution_mode      = "local"
+      project_id          = module.project["devops-course-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/devops-course-terraform-tfe"
     }
   }
 }
