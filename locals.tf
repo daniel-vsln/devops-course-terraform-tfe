5309e2a6937036d1db8e0c37ea9ee94d98bdaceb
Daniil Vasylenko <daniel.vsln@gmail.com>
2024-11-19 13:50:24 +0200

    feature/change aws region (#3)
    * feat: change aws region
    
    * feat: change azs

--- locals.tf
+++ locals.tf
@@ -29,7 +29,7 @@ locals {
           category = "terraform"
           hcl      = true
           key      = "azs"
-          value    = jsonencode(["eu-central-1"])
+          value    = jsonencode(["eu-central-1a", "eu-central-1b"])
         },
         {
           category = "terraform"
