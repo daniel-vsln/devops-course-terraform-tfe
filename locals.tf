ba07758e4e93a7e844a41bd5fa762d4f4acd7a03
Daniil Vasylenko <daniel.vsln@gmail.com>
2024-11-19 13:15:51 +0200

    feat: added workspaces (#1)

--- locals.tf
+++ locals.tf
@@ -18,6 +18,61 @@ locals {
       project_id          = module.project["devops-course-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/devops-course-terraform-github"
     }
+    "devops-course-aws-network" = {
+      description         = "Automation for AWS network resources."
+      execution_mode      = "remote"
+      project_id          = module.project["devops-course-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/devops-course-terraform-aws-network"
+
+      variables = [
+        {
+          category = "terraform"
+          hcl      = true
+          key      = "azs"
+          value    = jsonencode(["us-west-2a", "us-west-2b"])
+        },
+        {
+          category = "terraform"
+          key      = "cidr"
+          value    = "10.0.0.0/16"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "devops-course"
+        },
+      ]
+    }
+
+    "devops-course-aws-cluster-prod" = {
+      description         = "Automation for AWS cluster resources."
+      execution_mode      = "remote"
+      project_id          = module.project["devops-course-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/devops-course-terraform-aws-cluster"
+
+      variables = [
+        {
+          category = "terraform"
+          key      = "domain"
+          value    = "bid2win.click"
+        },
+        {
+          category = "terraform"
+          key      = "environment"
+          value    = "prod"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "devops-course-daniel-vsln"
+        },
+        {
+          category = "terraform"
+          key      = "vpc_name"
+          value    = "devops-course"
+        },
+      ]
+    }
   }
 }
 
